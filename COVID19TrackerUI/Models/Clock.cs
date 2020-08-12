7e82924d0a2a921d4c8ecbf8b93da93cb3441978
Brandon Weaver <a7xncob@gmail.com>
2020-08-12 17:18:27 -0400

    Add clock model.

---
+++ COVID19TrackerUI/Models/Clock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace COVID19TrackerUI.Models
+{
+    class Clock
+    {
+        private readonly DispatcherTimer dispatcherTimer;
+
+        public Clock(EventHandler<object> callback, int milliseconds)
+        {
+            this.dispatcherTimer = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromMilliseconds(milliseconds)
+            };
+            this.dispatcherTimer.Tick += callback;
+            this.dispatcherTimer.Start();
+        }
+    }
+}
