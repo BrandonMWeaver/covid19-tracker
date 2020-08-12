9b55e6ee233e272ee1e4008a6442bdc942835e48
Brandon Weaver <a7xncob@gmail.com>
2020-08-12 17:08:51 -0400

    Add statistics view model.

---
+++ COVID19TrackerUI/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using COVID19TrackerUI.Models;
+using COVID19TrackerUI.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COVID19TrackerUI.ViewModels
+{
+    class StatisticsViewModel : NotificationBase
+    {
+        private Statistics _statistics;
+
+        public Statistics Statistics
+        {
+            get { return this._statistics; }
+            set
+            {
+                this._statistics = value;
+                this.OnPropertyChanged(nameof(this.Statistics));
+            }
+        }
+    }
+}
